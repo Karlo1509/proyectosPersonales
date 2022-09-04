21ab7742fb0eee9ffb88c15aa404c35b3f6c5f9e
karlo1509 <karlocastillozapata@gmail.com>
2022-09-04 00:11:16 -0500

    Encripatado hecho, falta resolver problema del cambio

--- programacionBasica/challengesOne/encriptador/controlador.js
+++ programacionBasica/challengesOne/encriptador/controlador.js
@@ -1,92 +1,69 @@
-function colocarimagen(){
-    var campovacio = document.getElementById("textoresultado");
-    if (window.matchMedia("(min-width: 768px)").matches) {
-        campovacio.style.backgroundImage = 'url(images/Imagen.svg)'; 
-    } else {
-        campovacio.style.backgroundImage = 'none';
-    }
-}
 
-function validarContenido(){
-    var textoobtenido = document.getElementById("textoobtenido").value;
-    if (textoobtenido==""){
-        document.getElementById("textoobtenido").focus();
-        return false;
-    }
     
-    return true;
+
+window.onload= function(){
+    var caja1 = document.getElementById("caja1")
+    caja1.focus()
+    caja1.onkeyup = (actualizar)
     
-}
+} 
+    
+function actualizar() {
+    var caja1 = document.getElementById("caja1")
+    var caja2 = document.getElementById("caja2")
+    var texto1 = document.getElementById("texto1")
+    var texto2 = document.getElementById("texto2")
+    var intercambiar = document.getElementsByClassName("intercambiar")
 
-function limpiarenfocar(){
-    document.getElementById('textoobtenido').value = "";
-    document.getElementById("textoresultado").focus();
-}
-
-function encriptarTexto(){
-
-    var seguir = validarContenido();
-    if(seguir){
-
-        var textoobtenido = document.getElementById("textoobtenido").value;
-        var campovacio = document.getElementById("textoresultado");
-        //textoobtenido  = textoobtenido.toLowerCase();
-        campovacio.style.backgroundImage = "none";
-        
-        var texto = "";
-        
-        for(var i=0; i<textoobtenido.length; i++){
-            if(textoobtenido[i]!="a" && textoobtenido[i]!="e" && textoobtenido[i]!="i"&& textoobtenido[i]!="o" && textoobtenido[i]!="u")
-            {texto = texto + textoobtenido[i];}
-            if(textoobtenido[i]=="e"){texto = texto + "enter";}
-            if(textoobtenido[i]=="i"){texto = texto + "imes";}
-            if(textoobtenido[i]=="a"){texto = texto + "ai";}
-            if(textoobtenido[i]=="o"){texto = texto + "ober";}
-            if(textoobtenido[i]=="u"){texto = texto + "ufat";}
-            
+    if (caja1 != ""){
+        if (texto1.textContent == "Desencriptado"){
+            caja2.value = encriptar()
         }
-        
-        limpiarenfocar();
-        document.getElementById('textoresultado').innerText = texto;
-
-    }
-
-}
-function desencriptarTexto(){
-
-    var seguir = validarContenido();
-    let matrizcodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
-    
-    if(seguir){
-
-        var textoobtenido = document.getElementById("textoobtenido").value; 
-        var campovacio = document.getElementById("textoresultado");
-        //textoobtenido  = textoobtenido.toLowerCase();
-        campovacio.style.backgroundImage = "none";
-    
-        for( let i=0; i<matrizcodigo.length;i++){
-            if(textoobtenido.includes(matrizcodigo[i][1])){
-                textoobtenido=textoobtenido.replaceAll(matrizcodigo[i][1],matrizcodigo[i][0]);
-            }
+        if (texto1.textContent == "Encriptado"){
+            caja2.value = desencriptar()
         }
-
-        limpiarenfocar();
-        document.getElementById("textoresultado").innerText = textoobtenido;
     }
-
 }
 
-function copiarTexto(){
+function encriptar(){
+    var caja1 = document.getElementById("caja1")
+    var resultado = ""
 
-    let textoacopiar = document.querySelector("#textoresultado")
-    textoacopiar.select();
-    document.execCommand("copy");
-
-    document.getElementById('textoresultado').innerText = "";
-    limpiarenfocar();
-    document.getElementById("textoobtenido").focus();
-
-    colocarimagen();
+    for(var i = 0; i < caja1.value.length; i++){
+        if(caja1.value[i] != "a" 
+        && caja1.value[i] != "e" 
+        && caja1.value[i] != "i"
+        && caja1.value[i] != "o" 
+        && caja1.value[i] != "u")
+        {resultado = resultado + caja1.value[i];}
+        else {
+        if(caja1.value[i]=="e"){resultado = resultado + "enter"}
+        if(caja1.value[i]=="i"){resultado = resultado + "imes"}
+        if(caja1.value[i]=="a"){resultado = resultado + "ai"}
+        if(caja1.value[i]=="o"){resultado = resultado + "ober"}
+        if(caja1.value[i]=="u"){resultado = resultado + "ufat"}}
+    }
+    return resultado
 }
 
-window.onresize = colocarimagen;    
+function desencriptar(){
+    var caja1 = document.getElementById("caja1")
+    var resultado = ""
+
+    for(var i = 0; i < caja1.value.length; i++){
+        if(caja1.value[i] != "a" 
+        && caja1.value[i] != "e" 
+        && caja1.value[i] != "i"
+        && caja1.value[i] != "o" 
+        && caja1.value[i] != "u")
+        {resultado = resultado + caja1.value[i];}
+        else {
+        if(caja1.value[i]=="e"){resultado = resultado - "enter"}
+        if(caja1.value[i]=="i"){resultado = resultado - "imes"}
+        if(caja1.value[i]=="a"){resultado = resultado - "ai"}
+        if(caja1.value[i]=="o"){resultado = resultado - "ober"}
+        if(caja1.value[i]=="u"){resultado = resultado - "ufat"}}
+    }
+    return resultado
+}
+
