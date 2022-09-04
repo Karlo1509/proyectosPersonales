6bffe210f69467ef463a23954fd1207a106b7ed1
karlo1509 <karlocastillozapata@gmail.com>
2022-09-04 01:08:04 -0500

    primera version del desencriptador con algunas fallas que mejorar

--- programacionBasica/challengesOne/encriptador/controlador.js
+++ programacionBasica/challengesOne/encriptador/controlador.js
@@ -3,8 +3,10 @@
 
 window.onload= function(){
     var caja1 = document.getElementById("caja1")
+    var intercambiar = document.getElementById("intercambiar")
     caja1.focus()
     caja1.onkeyup = (actualizar)
+    intercambiar.onclick = function click(){intercambio();actualizar()}
     
 } 
     
@@ -13,8 +15,7 @@ function actualizar() {
     var caja2 = document.getElementById("caja2")
     var texto1 = document.getElementById("texto1")
     var texto2 = document.getElementById("texto2")
-    var intercambiar = document.getElementsByClassName("intercambiar")
-
+    var intercambiar = document.getElementById("intercambiar")
     if (caja1 != ""){
         if (texto1.textContent == "Desencriptado"){
             caja2.value = encriptar()
@@ -58,12 +59,41 @@ function desencriptar(){
         && caja1.value[i] != "u")
         {resultado = resultado + caja1.value[i];}
         else {
-        if(caja1.value[i]=="e"){resultado = resultado - "enter"}
-        if(caja1.value[i]=="i"){resultado = resultado - "imes"}
-        if(caja1.value[i]=="a"){resultado = resultado - "ai"}
-        if(caja1.value[i]=="o"){resultado = resultado - "ober"}
-        if(caja1.value[i]=="u"){resultado = resultado - "ufat"}}
+        if(caja1.value[i]=="e"){
+            resultado = resultado + "e";
+            i = i + 4 ;
+        }
+        if(caja1.value[i]=="i"){
+            resultado = resultado + "i";
+            i = i + 3 ;
+        }
+        if(caja1.value[i]=="a"){
+            resultado = resultado + "a";
+            i = i + 1 ;
+        }
+        if(caja1.value[i]=="o"){
+            resultado = resultado + "o";
+            i = i + 3 ;
+        }
+        if(caja1.value[i]=="u"){
+            resultado = resultado + "u";
+            i = i + 3 ;
+        }
+        }
     }
     return resultado
+}
+
+function intercambio() {
+    var caja1 = document.getElementById("caja1")
+    var caja2 = document.getElementById("caja2")
+    var texto1 = document.getElementById("texto1")
+    var texto2 = document.getElementById("texto2")
+    var copiartexto = ""
+    caja1.value = caja2.value
+    copiartexto = texto1.textContent
+    texto1.textContent = texto2.textContent
+    texto2.textContent = copiartexto
+    caja1.focus()
 }
 
